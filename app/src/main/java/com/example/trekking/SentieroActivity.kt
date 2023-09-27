3e6700fcc9f74a3ccddaf22aec102b10794ab811
Andrea Monti <monti9527@gmail.com>
2023-09-27 19:02:49 +0200

    Added some new maps

--- app/src/main/java/com/example/trekking/SentieroActivity.kt
+++ app/src/main/java/com/example/trekking/SentieroActivity.kt
@@ -1,5 +1,6 @@
 package com.example.trekking
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
@@ -9,30 +10,15 @@ import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 
 
-lateinit var Image_view: ImageView
-lateinit var Scale_class: ScaleGestureDetector
-var Factor_f:Float = 0F
-var X_f:Float = 0F
-var Y_f:Float = 0F
-var Dx_f:Float = 0F
-var Dy_f:Float = 0F
-val MaxZoom_f:Float = 4F
-val MinZoom_f:Float = 1.5F
-var FirstTime_b:Boolean = false
-var X0_f:Float = 0F
-var Y0_f:Float = 0F
-var Treshhold:Float = 20F
 
-val XMax_f:Float = 700F
-val YMax_f:Float = 1000F
 
 
 class SentieroActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         var trek:String = intent.getStringExtra("aa").toString()
-        if(trek == "Rifugio Gnutti e la Via Terzulli")
-            setContentView(R.layout.aamaps)
+        if(trek == "Rifugio Gnutti e \n la Via Terzulli")
+            setContentView(R.layout.adamello_trek)
         else if(trek == "Guglielmo da Zone")
             setContentView(R.layout.guglielmo_trek)
         else if(trek == "Corna Trentapassi")
@@ -87,84 +73,21 @@ class SentieroActivity : ComponentActivity() {
             setContentView(R.layout.sentierofiori_trek)
         else
             setContentView(R.layout.aamaps)
+
+        setContentView(R.layout.adamello_trek)
         super.onCreate(savedInstanceState)
-        Image_view = findViewById(R.id.aamaps)
-        Scale_class = ScaleGestureDetector(this,ScaleListener())
+
+        val passeggiatabtn = findViewById<TextView>(R.id.mappa_btn)
+
+
+        passeggiatabtn.setOnClickListener{
+            val intent: Intent = Intent(this, MapsActivity::class.java)
+            intent.putExtra("sentiero", trek)
+            startActivity(intent)
+        }
+
 
     }
 
-    public override fun onTouchEvent(event: MotionEvent): Boolean
-    {
-        if(event.action == MotionEvent.ACTION_DOWN)
-        {
-            X_f = event.getX()
-            Y_f = event.getY()
-        }
 
-        if(event.action == MotionEvent.ACTION_MOVE)
-        {
-            if(FirstTime_b == false)
-            {
-                X0_f = Image_view.x
-                Y0_f = Image_view.y
-                FirstTime_b = true
-            }
-            Dx_f = event.getX() - X_f
-            Dy_f = event.getY() - Y_f
-
-            Image_view.x = (Image_view.x + Dx_f*0.7).toFloat()
-            Image_view.y = (Image_view.y + Dy_f*0.7).toFloat()
-
-            if(Image_view.x > XMax_f || Image_view.x < -XMax_f )
-            {
-                if(Image_view.x > 0)
-                {
-                    Image_view.x = XMax_f-Treshhold
-                }
-                else
-                {
-                    Image_view.x = -XMax_f+Treshhold
-
-                }
-            }
-            if(Image_view.y > YMax_f || Image_view.y < -YMax_f)
-            {
-                if(Image_view.y > 0)
-                {
-                    Image_view.y = YMax_f-Treshhold
-                }
-                else
-                {
-                    Image_view.y = -YMax_f+Treshhold
-                }
-            }
-            X_f = event.getX()
-            Y_f = event.getY()
-
-            Log.d("x =", "Value: " + Image_view.y);
-        }
-        Scale_class.onTouchEvent(event)
-        return super.onTouchEvent(event)
-    }
-
-    public class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener()
-    {
-        public override fun onScale(gest:ScaleGestureDetector): Boolean
-        {
-            Factor_f += (gest.scaleFactor-1)
-            if(Factor_f > MaxZoom_f)
-                Factor_f = MaxZoom_f
-            if(Factor_f < MinZoom_f)
-                Factor_f = MinZoom_f
-            Image_view.scaleX = Factor_f
-            Image_view.scaleY = Factor_f
-            return true
-        }
-
-        public override fun onScaleBegin(gest:ScaleGestureDetector): Boolean
-        {
-            return true
-        }
-
-    }
 }
